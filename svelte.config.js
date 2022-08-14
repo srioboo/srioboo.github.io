6aceb4ffcddcbc9018ec24f8ec9c324086302a8a
Salvador Rioboo Naranjo <salrio74@gmail.com>
2022-08-14 21:03:44 +0200

    fix: correciones para que github pages funcione

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-github';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,14 +11,19 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
+			pages: 'docs',
+			assets: 'docs',
+			domain: null,
+			jekyll: false,
+			fallback: null,
+			precompress: false
 		}),
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
+		paths: {
+			base: dev ? '' : '/srioboo.github.io',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 	}
 };
 
