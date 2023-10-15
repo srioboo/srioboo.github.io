71b83834df17346faa054f376873f8f725aea2ae
name <mail>
2023-10-15 19:14:10 +0200

    fix: fix build

--- src/lib/components/sections/Portfolio.svelte
+++ src/lib/components/sections/Portfolio.svelte
@@ -14,13 +14,13 @@
         <div class="row">
             <div class="col-4 col-12-mobile">
                 <article class="item">
-                    <a href="#" class="image fit"><img src="../../../static/images/pic02.jpg" alt="" /></a>
+                    <a href="/" class="image fit"><img src="../../../static/images/pic02.jpg" alt="" /></a>
                     <header>
                         <h3>Ipsum Feugiat</h3>
                     </header>
                 </article>
                 <article class="item">
-                    <a href="#" class="image fit"><img src="../../../static/images/pic03.jpg" alt="" /></a>
+                    <a href="/" class="image fit"><img src="../../../static/images/pic03.jpg" alt="" /></a>
                     <header>
                         <h3>Rhoncus Semper</h3>
                     </header>
@@ -28,13 +28,13 @@
             </div>
             <div class="col-4 col-12-mobile">
                 <article class="item">
-                    <a href="#" class="image fit"><img src="../../../static/images/pic04.jpg" alt="" /></a>
+                    <a href="/" class="image fit"><img src="../../../static/images/pic04.jpg" alt="" /></a>
                     <header>
                         <h3>Magna Nullam</h3>
                     </header>
                 </article>
                 <article class="item">
-                    <a href="#" class="image fit"><img src="../../../static/images/pic05.jpg" alt="" /></a>
+                    <a href="/" class="image fit"><img src="../../../static/images/pic05.jpg" alt="" /></a>
                     <header>
                         <h3>Natoque Vitae</h3>
                     </header>
@@ -42,13 +42,13 @@
             </div>
             <div class="col-4 col-12-mobile">
                 <article class="item">
-                    <a href="#" class="image fit"><img src="../../../static/images/pic06.jpg" alt="" /></a>
+                    <a href="/" class="image fit"><img src="../../../static/images/pic06.jpg" alt="" /></a>
                     <header>
                         <h3>Dolor Penatibus</h3>
                     </header>
                 </article>
                 <article class="item">
-                    <a href="#" class="image fit"><img src="../../../static/images/pic07.jpg" alt="" /></a>
+                    <a href="/" class="image fit"><img src="../../../static/images/pic07.jpg" alt="" /></a>
                     <header>
                         <h3>Orci Convallis</h3>
                     </header>
