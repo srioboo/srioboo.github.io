8137b2c843403803abed9b375e168b35c7de470e
SRN <salrio74@gmail.com>
2024-10-21 09:18:28 +0200

    feat: first step to i18

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+// show default language
+export const showDefaultLang = false;
+
+export const languages = {
+	en: 'English',
+	es: 'Español'
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+	en: {
+		'nav.home': 'Home',
+		'nav.about': 'About',
+		'nav.twitter': 'Twitter'
+	},
+	es: {
+		'nav.home': 'Inicio',
+		'nav.about': 'Acerca de'
+	}
+} as const;
